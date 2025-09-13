947195eef7c8ec1941653e8d758035195f2cdfb5
Ezekiel <ezekielakinfenwa98@gmail.com>
2025-09-13 22:11:44 +0100

    Built version 1.0.0 of the website
    Added .gitignore to watch list of changes

---
+++ frontend/src/lib/BlogCard.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import type { Post } from "../utils/funcs";
+    import { formatDate } from "../utils/funcs";
+
+    let { blog }: { blog: Post } = $props();
+</script>
+
+<article>
+    <a href="/blogs/{blog.short_title}" aria-label="Link to article"></a>
+    <h4>{blog.title}</h4>
+    <p>
+        {blog.subtitle}
+    </p>
+    <span>Written on {formatDate(new Date(blog.created))}</span>
+</article>
+
+<style>
+    p {
+        color: #212121;
+        font-size: 0.95rem;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        p {
+            color: #a0acc7;
+        }
+    }
+
+    article {
+        position: relative;
+    }
+
+    a {
+        position: absolute;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    a:hover + h4 {
+        text-decoration: underline;
+    }
+
+    span {
+        font-size: 0.875rem;
+    }
+</style>
---
+++ frontend/src/pages/Blogs.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { getAllBlogs } from "../utils/funcs";
+    import BlogCard from "../lib/BlogCard.svelte";
+    import Icon from "@iconify/svelte";
+
+    let allBlogs = $state(getAllBlogs());
+</script>
+
+<section>
+    <h2 id="blog-heading">Blogs <Icon icon="tabler:star" /></h2>
+    <p>Some of the stuff I've written.</p>
+</section>
+
+<section aria-label="Blog List" class="blog-list">
+    {#await allBlogs then data}
+        {#each data as blog, _ (_)}
+            <BlogCard {blog} />
+        {/each}
+    {/await}
+</section>
+
+<style>
+
+    h2 {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+    }
+
+    section.blog-list {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        margin-block-start: 4rem;
+    }
+</style>
